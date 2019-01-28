c55b201b34060c3300ca5a03ebacb42cf6f8c265
Marten Seemann <martenseemann@gmail.com>
2019-01-28 20:50:13 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+func main() {
+	file, err := os.Create("rtt.txt")
+	if err != nil {
+		panic(err)
+	}
+	if err := run(file); err != nil {
+		panic(err)
+	}
+}
+
+func run(output io.Writer) error {
+	const runTime = 3 * time.Second
+	const interval = time.Millisecond
+	const numPackets = uint64(runTime / interval)
+
+	var mutex sync.Mutex
+
+	sendTimes := make(map[uint64]time.Time)
+
+	saddr, err := net.ResolveUDPAddr("udp", "localhost:0")
+	if err != nil {
+		return err
+	}
+	sconn, err := net.ListenUDP("udp", saddr)
+	if err != nil {
+		return err
+	}
+
+	done := make(chan struct{})
+	b := make([]byte, 8)
+	go func() {
+		for i := uint64(0); i < numPackets; i++ {
+			n, _, err := sconn.ReadFrom(b)
+			if n != 8 {
+				panic("short read")
+			}
+			if err != nil {
+				panic(err)
+			}
+			now := time.Now()
+			pn := binary.BigEndian.Uint64(b)
+			mutex.Lock()
+			sendTime := sendTimes[pn]
+			mutex.Unlock()
+			fmt.Fprintf(output, "%d %d\n", pn, uint64(now.Sub(sendTime)/time.Nanosecond))
+		}
+
+		close(done)
+	}()
+
+	caddr, err := net.ResolveUDPAddr("udp", "localhost:0")
+	if err != nil {
+		return err
+	}
+	cconn, err := net.DialUDP("udp", caddr, sconn.LocalAddr().(*net.UDPAddr))
+	if err != nil {
+		return err
+	}
+
+	p := make([]byte, 8)
+	for i := uint64(0); i < numPackets; i++ {
+		binary.BigEndian.PutUint64(p, i)
+		if _, err := cconn.Write(p); err != nil {
+			return err
+		}
+		now := time.Now()
+		mutex.Lock()
+		sendTimes[i] = now
+		mutex.Unlock()
+
+		time.Sleep(interval)
+	}
+
+	<-done
+	return nil
+}
