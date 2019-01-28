ba70672df3cd5089ac60c51a1054db5797f39a5a
Marten Seemann <martenseemann@gmail.com>
2019-01-28 22:33:54 +0900

    simplify proxy by just proxying one direction

--- proxy/main.go
+++ proxy/main.go
@@ -26,77 +26,28 @@ func run(port, serverPort int, delay time.Duration) error {
 		return err
 	}
 
-	saddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("localhost:%d", port))
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("localhost:%d", port))
 	if err != nil {
 		return err
 	}
-	sconn, err := net.ListenUDP("udp", saddr)
+	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Proxying connections %s <-> %s (%s delay)\n", sconn.LocalAddr(), receiverAddr, delay)
+	fmt.Printf("Proxying connections %s <-> %s (%s delay)\n", conn.LocalAddr(), receiverAddr, delay)
 
-	caddr, err := net.ResolveUDPAddr("udp", "localhost:0")
-	if err != nil {
-		return err
-	}
-	cconn, err := net.ListenUDP("udp", caddr)
-	if err != nil {
-		return err
-	}
-
-	clientAddrChan := make(chan *net.UDPAddr, 1)
-	go func() {
-		if err := runUpstream(sconn, cconn, receiverAddr, clientAddrChan, delay); err != nil {
-			panic(err)
-		}
-	}()
-
-	go func() {
-		if err := runDownstream(sconn, cconn, clientAddrChan, delay); err != nil {
-			panic(err)
-		}
-	}()
-
-	select {}
-}
-
-func runUpstream(sconn, cconn *net.UDPConn, receiverAddr *net.UDPAddr, clientAddrChan chan<- *net.UDPAddr, delay time.Duration) error {
-	var hasClientAddr bool
 	for {
 		b := make([]byte, 8)
-		n, addr, err := sconn.ReadFromUDP(b)
+		n, _, err := conn.ReadFromUDP(b)
 		if err != nil {
 			return err
 		}
 		if n != 8 {
 			return errors.New("small read")
 		}
-		if !hasClientAddr {
-			clientAddrChan <- addr
-			hasClientAddr = true
-		}
-		time.AfterFunc(delay, func() {
-			if _, err := cconn.WriteTo(b, receiverAddr); err != nil {
-				panic(err)
-			}
-		})
-	}
-}
 
-func runDownstream(sconn, cconn *net.UDPConn, clientAddrChan <-chan *net.UDPAddr, delay time.Duration) error {
-	senderAddr := <-clientAddrChan
-	for {
-		b := make([]byte, 8)
-		n, _, err := cconn.ReadFromUDP(b)
-		if err != nil {
-			return err
-		}
-		if n != 8 {
-			return errors.New("small read")
-		}
 		time.AfterFunc(delay, func() {
-			if _, err := sconn.WriteTo(b, senderAddr); err != nil {
+			if _, err := conn.WriteTo(b, receiverAddr); err != nil {
 				panic(err)
 			}
 		})
